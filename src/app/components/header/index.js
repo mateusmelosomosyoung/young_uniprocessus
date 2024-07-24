c95cba3ed1eec24916f5debc788bd759c6204756
Mateus <mateusfpmelodev@gmail.com>
2024-07-24 12:42:48 -0300

    header sem form

---
+++ src/app/components/header/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from './header.module.css'
+
+const HeaderModule = () => {
+
+    return(<>
+        <div className={styles.hdrd}>
+            <div>
+
+            </div>
+        </div>
+    </>)
+}
+
+export default HeaderModule
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Image from "next/image"
 import styles from "./page.module.css"
 import Navbar from "./components/navbar"
+import HeaderModule from "./components/header"
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <Navbar />
-      </div>
+      <Navbar />
+      <HeaderModule />
     </main>
   )
 }
